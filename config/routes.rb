fb0aceeaa8f121699e4c0ca11cf5d56f6a0ca9a0
Jurgens Smit <contact@jurgenssmit.com>
2013-05-25 23:21:06 +1000

    add background and testing landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 MusicLessons::Application.routes.draw do
-  
+  get "landing_page/index"
 
-  match 'teacher_list' => 'teacher_list#index'
-
-  get 'tags/:tag', to: 'users#show', as: :tag
-  
-
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users
-  resources :users do
-    resources :events
-  end
-  match "/auth/google_login/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+match 'teacher_list' => 'teacher_list#index'
+get 'tags/:tag', to: 'users#show', as: :tag
+authenticated :user do
+root :to => 'home#index'
+end
+root :to => "home#index"
+devise_for :users
+resources :users do
+resources :events
+end
+match "/auth/google_login/callback" => "sessions#create"
+match "/signout" => "sessions#destroy", :as => :signout
 end
