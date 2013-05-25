3502e6be385c78c86e85caff20d35c514111864e
Jurgens Smit <contact@jurgenssmit.com>
2013-05-25 23:37:36 +1000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 MusicLessons::Application.routes.draw do
-<<<<<<< HEAD
+
   get "landing_page/index"
-=======
   
   resources :messages do
     get :autocomplete_user_name, :on => :collection
@@ -15,7 +14,7 @@ MusicLessons::Application.routes.draw do
   match '/sent' => 'messages#sent'
   match '/trash' => 'messages#trash'
   match '/admin' => 'admin#index'
->>>>>>> 4e1d2562cf5d7a3b3818fd34aaebd632024f5e2b
+
 
 match 'teacher_list' => 'teacher_list#index'
 get 'tags/:tag', to: 'users#show', as: :tag
